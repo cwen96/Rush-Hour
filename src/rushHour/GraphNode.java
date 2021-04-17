a213147bbf5025ec97ea66d8d5779d1bbfecbfd4
Kenichiro Morikawa <kenichiromorikawa@KenichironoMacBook-Pro.local>
2021-04-16 20:50:31 -0600

    diffBoard

--- src/rushHour/GraphNode.java
+++ src/rushHour/GraphNode.java
@@ -23,21 +23,59 @@ public class GraphNode {
 
     }
 
+    public GraphNode() {
+
+    }
+
     public String boardDiff(ArrayList<String> board1, ArrayList<String> board2) {
-        String diff;
-        char movedCar;
-        for (int i = 0; i < board1.size(); i++) {
-            char[] compare1 = board1.get(i).toCharArray();
-            char[] compare2 = board2.get(i).toCharArray();
-            for (int j = 0; j < board1.size(); j++) {
-                if (compare1[j] != compare2[j]) {
-                    movedCar = (compare1[j] == '.') ? compare2[j] : compare1[j];
+        //if board2 is null, then no moves; therefore, diff is null;
+        String diff = null;
+        char movedCar = ' ';
+        char dir = ' ';
+        boolean checkDode = false;
+        if(board2 != null) {
+            while(!checkDode)
+                for (int i = 0; i < board1.size(); i++) {
+                    char[] compare1 = board1.get(i).toCharArray();
+                    char[] compare2 = board2.get(i).toCharArray();
+                    for (int j = 0; j < board1.size(); j++) {
+                        if (compare1[j] != compare2[j]) {
+                            if (compare2[j] == '.') {
+                                movedCar = compare1[j];
+                                if(!checkDode) {
+                                    if (compare2[j + 1] == movedCar) {
+                                        dir = 'R';
+                                        checkDode = true;
+                                        break;
+                                    }
+                                    if (compare1[j] == movedCar) {
+                                        dir = 'D';
+                                        checkDode = true;
+                                    }
+                                }
+                            }
+                            if (compare2[j] != '.') {
+                                movedCar = compare2[j];
+                                if(!checkDode) {
+                                    if (compare1[j + 1] == movedCar) {
+                                        dir = 'L';
+                                        checkDode = true;
+                                        break;
+                                    }
+                                    if (compare1[j] != movedCar) {
+                                        dir = 'U';
+                                        checkDode = true;
+                                    }
+                                }
+                            }
+                        }
+                        if(checkDode) {
+                            break;
+                        }
+                    }
                 }
-
-            }
+            diff = movedCar + "" + dir;
         }
-
-
         return diff;
     }
 
